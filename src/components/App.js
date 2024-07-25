c062bdedeaad916431bc2e3bcca3ec708f837c50
Nikki Tiwari <nikkitiwari915@gmail.com>
2024-07-25 19:44:46 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import 'regenerator-runtime/runtime';
 
 const App = () => {
+  const [output, setOutput] = useState();
+  const [fetching, setFetching] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const api = "https://dummyjson.com/products";
+    const fetchData =  async () => {
+      try {
+        const response = await axios(api);
+        setOutput(response.data);
+        setFetching(false);
+      } catch (error) {
+        setError(`An error occurred: ${error.message}`);
+        setFetching(false);
+    }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <u>Output : </u>
+      {fetching ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <div>
+          <h1>Data Fetched from API</h1>
+          <pre>{JSON.stringify(output, null, 2)}</pre>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
